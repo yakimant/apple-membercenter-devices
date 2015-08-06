fb32d07ab0a11bf4a6df6e68b2773753962fcc19
Anton Yakimov <a.yakimov@corp.badoo.com>
2015-08-06 18:07:44 +0300

    Update the script according to Member Center changes.

--- get_the_current_list.js
+++ get_the_current_list.js
@@ -1,13 +1,14 @@
 var utils = require('utils');
 var fs = require('fs');
 
-var select_team_page = 'https://developer.apple.com/membercenter/selectTeam.action';
+var select_team_page = 'https://developer.apple.com/account/selectTeam.action';
 var devices_list_page = 'https://developer.apple.com/account/ios/device/deviceList.action';
 var devices_create_page = 'https://developer.apple.com/account/ios/device/deviceCreate.action';
 
 var teams;
 var devices = {};
 var devices_all = {};
+var devices_to_upload = {};
 
 var casper = require('casper').create({
   pageSettings: {
@@ -29,50 +30,81 @@ casper.on('remote.message', function(msg) {
 
 casper.start();
 
-configFile = fs.read('./config.json');
 casper.then(function() {
-    config = JSON.parse(configFile);
+  configFile = fs.read('./config.json');
+  config = JSON.parse(configFile);
+  this.options.waitTimeout = config.waitTimeout;
+  this.options.timeout = config.timeout;
+  devices_hardcoded_names = config.devices_hardcoded_names;
 });
 
-var devices_hardcoded_names = config.devices_hardcoded_names;
-
-casper.thenOpen(select_team_page, function(response) { //).waitForUrl(/login/, function() { 
-  // require('utils').dump(response);
-  // this.echo(this.getHTML());
+casper.thenOpen(select_team_page, function openSelectTeamPage(response) {
   this.fillSelectors('form#command', {
     'input[name="appleId"]':          config.appleid,
     'input[name="accountPassword"]':  config.password
   }, true);
-});
-
-casper.then(function() {
+}).then(function getTeamsList() {
   teams = this.evaluate(function() {
     var teams = [];
-    var team_nodes = document.querySelectorAll('#teams option');
-    for (var i = 0; i < team_nodes.length; i++) {
-      teams.push(team_nodes[i].value);
-    }
+    $('.input').children('.team-value').each(function () {
+      teams.push({
+        'id': $(this).children('.radio').val(),
+        'name': $.trim($(this).children('.label-primary').text()),
+        'program': $(this).children('.label-secondary').text()
+      });
+    });
     return teams;
   });
 });
 
 casper.then(function() {
-  this.each(teams, function(self, team) {
-    this.thenOpen(select_team_page, function(response) {
-      this.fillSelectors('form#saveTeamSelection', {
-        'select[name="memberDisplayId"]':  team,
-      }, true);
-      this.thenOpen(devices_list_page, function(response) {
-        var team_devices = this.evaluate(function() {
+  this.eachThen(teams, function(team_data) {
+    var team = team_data.data;
+    var status = 'ok';
+    var team_devices = {};
+    this.thenOpen(select_team_page, function openSelectTeamPage() {
+    }).waitForSelector('form#saveTeamSelection', function waitForSelectTeamPage() {
+    }, function() {
+      fs.write(config.logfile, 'ERROR: Could not open select team page for team: ' + team.id + '\n', 'a');
+      status = 'fail';
+    }).then(function selectTeam() {
+      if (status === 'ok') {
+        this.fillSelectors('form#saveTeamSelection', {
+          'input[name="memberDisplayId"]':  team.id,
+        }, true);
+      }
+    }).waitForSelector('#content', function waitForTeamSelected() {
+    }, function() {
+      fs.write(config.logfile, 'ERROR: Could not select team: ' + team.id + '\n', 'a');
+      status = 'fail';
+    }).thenOpen(devices_list_page, function openDevicesListPage() {
+    }).waitForSelector(".innercontent", function waitForDevicesListPage() {
+    }).then(function checkDevicesListPage() {
+      if (status === 'ok') {
+        status = this.evaluate(function () {
+          if ($('.innercontent').find('.overview').length === 1) {
+            return 'fail';
+          }
+          if ($('.innercontent').find('.no-items-content').length === 1) {
+            return 'empty';
+          }
+          return 'ok';
+        });
+      }
+    }).then(function getDevicesData() {
+      if (status === 'ok') {
+        team_devices = this.evaluate(function() {
           var team_devices = {};
-          var device_name_nodes = document.querySelectorAll('#grid-table td[aria-describedby="grid-table_name"]');
-          var device_uuid_nodes = document.querySelectorAll('#grid-table td[aria-describedby="grid-table_deviceNumber"]');
+          var device_name_nodes = $('#grid-table').find('td[aria-describedby="grid-table_name"]');
+          var device_uuid_nodes = $('#grid-table').find('td[aria-describedby="grid-table_deviceNumber"]');
           var uuid
           var name;
-          for (var i = 0; i < device_uuid_nodes.length; i++) {
+          for (var i = 0, devices_count = device_uuid_nodes.length; i < devices_count; i++) {
             uuid = device_uuid_nodes[i].innerHTML;
             name = device_name_nodes[i].innerHTML;
             team_devices[uuid] = name;
+            // console.log(uuid);
+            // console.log(name);
           }
           return team_devices;
         });
@@ -81,59 +113,103 @@ casper.then(function() {
             team_devices[uuid] = devices_hardcoded_names[uuid];
           }
         }
-        devices[team] = team_devices;
+        devices[team.id] = team_devices;
         utils.mergeObjects(devices_all, team_devices);
-      });
+      }
     });
   });
 });
-var devices_to_upload = {};
 
 casper.then(function() {
   for (team in devices) {
     devices_to_upload[team] = {};
+    devices_to_upload[team]['deviceNames'] = '';
+    devices_to_upload[team]['deviceNumbers'] = '';
     fs.write(team+'.txt', '{UUID}\t{NAME}\n', 'w');
     for (uuid in devices_all) {
       if (!devices[team].hasOwnProperty(uuid)){
         fs.write(team+'.txt', uuid + '\t' + devices_all[uuid] + '\n', 'a');
         devices_to_upload[team][uuid] = devices_all[uuid];
+        devices_to_upload[team]['deviceNames'] += devices_all[uuid] + ',';
+        devices_to_upload[team]['deviceNumbers'] += uuid + ',';
       }
     }
   }
 });
 
 
-casper.then(function() {
-  this.each(teams, function(self, team) {
-    this.thenOpen(select_team_page, function(response) {
-      console.log('-- Adding devices to following team: ' + team);
-      this.fillSelectors('form#saveTeamSelection', {
-        'select[name="memberDisplayId"]':  team,
-      }, true);
-      this.thenOpen(devices_create_page, function(response) {
-      }).waitForSelector('input#register-multiple', function() {
-        this.click('input#register-multiple');
-        this.fill('form#deviceImport', {
-          'upload': team+'.txt'
-        }, true);
-        this.waitForSelector('form#deviceImportSave', function() {
-          this.fill('form#deviceImportSave', {
-          }, true);
-          utils.dump(devices_to_upload[team]);
-        });
-        this.waitForText('Registration complete.', function() {
-          this.capture(team+'.png');
-          console.log('Successfully imported\n');
-        }, function() {
-          this.capture(team+'.png');
-          console.log('FATAL: Import failed\n');
-        });
+casper.then(function addDevices() {
+  this.eachThen(teams, function(team_data) {
+    var status = 'ok';
+    var team = team_data.data;
+    console.log('-- Adding devices to following team: ' + team.name);
+    this.thenOpen(select_team_page, function openSelectTeamPage() {
+    }).waitForSelector('form#saveTeamSelection', function waitForSelectTeamPage() {
       }, function() {
-        this.capture(team+'.png');
-        console.log('FATAL: Can\'t open the teams devices page.\n');
-      });
+        fs.write(config.logfile, 'ERROR: Could not open select team page for team: ' + team.id + '\n', 'a');
+        status = 'fail';
+    }).then(function selectTeam() {
+      if (status === 'ok') {
+        this.fillSelectors('form#saveTeamSelection', {
+          'input[name="memberDisplayId"]':  team.id,
+        }, true);
+      }
+    }).waitForSelector('#content', function waitForTeamSelected() {
+    }, function() {
+      fs.write(config.logfile, 'ERROR: Could not select team: ' + team.id + '\n', 'a');
+      status = 'fail';
+    }).thenOpen(devices_create_page, function openDevicesCreatePage() {
+    }).waitForSelector(".innercontent", function waitForDevicesCreatePage() {
+    }, function() {
+      fs.write(config.logfile, 'ERROR: Could not open provision certificates page for team ' + team.id + '\n', 'a');
+      status = 'fail';
+    }).then(function checkDevicesCreatePage() {
+      if (status === 'ok') {
+        status = this.evaluate(function () {
+          if ($('.innercontent').find('.overview').length === 1) {
+            return 'fail';
+          }
+          if ($('.innercontent').find('.no-items-content').length === 1) {
+            return 'empty';
+          }
+          return 'ok';
+        });
+      }
+    }).then(function selectMultipleDevicesUpload() {
+      if (status === 'ok') {
+        this.click('input#register-multiple');
+      }
+    // }).waitForSelector('form#deviceImport .file-input[disabled!=disabled]', function uploadDevicesList() {
+    }).wait(1000, function uploadDevicesList() {
+      this.fillSelectors('form#deviceImport', {
+        'input[name="upload"]': team.id+'.txt',
+        'input[name="deviceNames"]': devices_to_upload[team.id]['deviceNames'],
+        'input[name="deviceNumbers"]': devices_to_upload[team.id]['deviceNumbers'],
+      }, true);
+    }).waitForSelector('form#deviceImportSave', function waitForConfirmPage() {
+    }, function() {
+      // fs.write(config.logfile, 'ERROR: Could not open provision certificates page for team ' + team.id + '\n', 'a');
+      status = 'fail';
+    }).then(function confirmUpload() {
+      if (status === 'ok') {
+        utils.dump(devices_to_upload[team.id]);
+        this.fill('form#deviceImportSave', {
+        }, true);
+      }
+    }).waitForText('Registration complete.', function importSucceed() {
+      this.capture(team.id+'.png');
+      console.log('Successfully imported\n');
+    }, function importFailed() {
+      this.capture(team.id+'.png');
+      console.log('FATAL: Import failed\n');
     });
+        // this.capture(team+'.png');
+        // console.log('FATAL: Can\'t open the teams devices page.\n');
   });
 });
+
+// casper.then(function() {
+//   utils.dump(devices_to_upload);
+// });
 
 casper.run();
